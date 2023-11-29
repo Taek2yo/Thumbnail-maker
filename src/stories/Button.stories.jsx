8e6c8d76bb966be6de3cdf8344b93eac2d67fb45
Mintaek2 <789rkd@naver.com>
2023-11-29 15:56:46 +0900

    test : Button test

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
-import Button from '../Buttons';
+import Button from '../components/atoms/Button';
 
 export default {
 	title: 'Button',
 	component: Button,
+	argTypes: { onClick: { action: 'clicked' } },
 };
 
-function Template(args) {
-	return <Button args={args} />;
-}
+const Template = (args) => <Button {...args} />;
 
-export const Basic = Template.bind({});
+export const Default = Template.bind({});
+Default.args = {
+	name: 'default',
+	id: 0,
+	$active: false,
+};
+
+export const GradientRandom = Template.bind({});
+GradientRandom.args = {
+	...Default.args,
+	name: '그래디언트 랜덤',
+	id: 1,
+};
+
+export const SolidRandom = Template.bind({});
+SolidRandom.args = {
+	...Default.args,
+	name: '단색 랜덤',
+	id: 2,
+};
+
+export const OpenModalImageURL = Template.bind({});
+OpenModalImageURL.args = {
+	...Default.args,
+	name: '이미지 URL 입력하기',
+	id: 3,
+};
