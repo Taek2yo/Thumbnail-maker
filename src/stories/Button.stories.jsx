21895f63ed6e9825599af0e62f3258335b4caed4
Mintaek2 <789rkd@naver.com>
2023-12-01 17:23:01 +0900

    test : Button

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 import Button from '../components/atoms/Button';
 
 export default {
 	title: 'atoms/Button',
 	component: Button,
+	decorators: [withKnobs],
 	argTypes: {
 		onClick: { action: 'clicked' },
 	},
@@ -14,6 +15,6 @@ const Template = (args) => <Button {...args} />;
 
 export const DefaultButton = Template.bind({});
 DefaultButton.args = {
-	name: 'default',
-	$active: false,
+	name: text('text', 'default'),
+	$active: boolean('$active', false),
 };
